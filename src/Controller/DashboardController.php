9586181381acf80a49d31fcf789c2c367a6a704e
Louis Sicard <lsicard@core-techs.fr>
2021-03-04 14:45:03 +0100

    update dependencies + Minor improvements

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -15,10 +15,12 @@ class DashboardController extends AdimeoDataSuiteController
       $info = $this->getIndexManager()->getIndicesInfo($this->buildSecurityContext());
 
       $serverInfo = $this->getIndexManager()->getServerInfo()['server_info'];
+      $clusterHealth = $this->getIndexManager()->getServerClient()->clusterHealth();
 
     } catch (ServerClientException $ex) {
       $info = null;
       $serverInfo = null;
+      $clusterHealth = null;
       $noMenu = true;
     }
 
@@ -26,6 +28,7 @@ class DashboardController extends AdimeoDataSuiteController
       'title' => $this->get('translator')->trans('Welcome to Adimeo Data Suite'),
       'info' => $info,
       'server_info' => $serverInfo,
+      'cluster_health' => $clusterHealth,
       'main_menu_item' => 'home',
       'no_menu' => isset($noMenu) && $noMenu ? true : false,
     ));
